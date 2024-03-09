6578db8d441e22d92aeb5708647814bbc37fbcf9
Szymon <edbsiedlce@gmail.com>
2024-03-09 07:59:39 +0100

    Create interfaces and const

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+interface Experience {
+    year: number;
+    description: string;
+}
+
+interface CVData {
+    photo: string;
+    name: string;
+    lastname: string;
+    position: string;
+    experience: Experience[];
+    education: [];
+}
+
+const myCV: CVData = {
+    photo: 'szymon.jpg',
+    name: 'Szymon',
+    lastName: 'Maciołek',
+    position: 'Teacher',
+    experience: [
+        {
+       year: 2005,
+        description: 'ZSP nr 2 w Siedlcach'
+        },{
+        year: 2019, description: 'SCDiDN w Siedlcach'
+        }
+    ],
+    education: [
+        '1998 - I Lo w Siedlcach - matura',
+        '2003 - Akademia Podlaska w Siedlcach - magister',
+        '2018 - Uniwersytet w Siedlcach - doktor'
+    ]
+}
+
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <h1>hello</h1>
+
 
   </React.StrictMode>,
 )
