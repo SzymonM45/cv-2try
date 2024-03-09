584b18d4e7f60227a5cbc1ddcbb3114e8a060ea9
Szymon <edbsiedlce@gmail.com>
2024-03-09 08:20:23 +0100

    built create elements

--- src/main.tsx
+++ src/main.tsx
@@ -9,10 +9,10 @@ interface Experience {
 interface CVData {
     photo: string;
     name: string;
-    lastname: string;
+    lastName: string;
     position: string;
     experience: Experience[];
-    education: [];
+    education: string[];
 }
 
 const myCV: CVData = {
@@ -21,12 +21,10 @@ const myCV: CVData = {
     lastName: 'Maciołek',
     position: 'Teacher',
     experience: [
-        {
-       year: 2005,
-        description: 'ZSP nr 2 w Siedlcach'
-        },{
-        year: 2019, description: 'SCDiDN w Siedlcach'
-        }
+        { year: 1998, description: 'Securitas Polska'},
+        { year: 2004, description: 'Astra Zeneca'},
+        {year: 2005, description: 'ZSP nr 2 w Siedlcach'},
+        {year: 2019, description: 'SCDiDN w Siedlcach'}
     ],
     education: [
         '1998 - I Lo w Siedlcach - matura',
@@ -39,7 +37,26 @@ const myCV: CVData = {
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+      <main>
+          <h1> CV {myCV.name} {myCV.lastName}</h1>
+          <aside>
+              <h2>Podstawowe dane</h2>
+              <img src={myCV.photo} alt={` Photo of ${myCV.name} ${myCV.lastName}`}/>
+              <p>{myCV.name} {myCV.lastName}</p>
+              <small>{myCV.position}</small>
+          </aside>
+          <section>
+              <h2>Doświadczenie</h2>
+              <ul>
+                  {myCV.experience.map(experience => <li key={experience.year}>{experience.year} - {experience.description}</li>)}
+              </ul>
+              <h2>Wykształcenie</h2>
+              <ul>
+                  {myCV.education.map((education, index) => <li key={index}>{education}</li>)}
+              </ul>
 
+          </section>
+      </main>
 
   </React.StrictMode>,
 )
