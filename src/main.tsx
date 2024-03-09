0925bed43dad5ac7230100708059e7fcadb0a98a
Szymon <edbsiedlce@gmail.com>
2024-03-09 07:41:55 +0100

    initialization of project

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+      <h1>hello</h1>
+
+  </React.StrictMode>,
+)
